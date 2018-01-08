474a4bf67ee9ac74b9c139cbbb508dc375bfce48
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-01-08 17:44:02 +0000

    25p1 completed

--- AoC/2017/25.py
+++ AoC/2017/25.py
@@ -1,3 +1,4 @@
+import collections
 sample1 ='''Begin in state A.
 Perform a diagnostic checksum after 6 steps.
 
@@ -32,26 +33,63 @@ def parseinput(inlist):
     for state in statelist:
         statedict = {}
         statedict['name'] = state[0]
-        zerowrite = state.split('\n')[2][-2]
+        zerowrite = (int)(state.split('\n')[2][-2])
         zerolr = (state.split('\n')[3]).split(' ')[-1][:-1]
-        zeronext = state.split('\n')[4][:-2]
+        zeronext = state.split('\n')[4][-2]
+        onewrite =  (int)(state.split('\n')[6][-2])
+        onelr = (state.split('\n')[7]).split(' ')[-1][:-1]
+        onenext = state.split('\n')[8][-2]
         statedict['zero'] = {'write':zerowrite,'dir':zerolr, 'next':zeronext} 
-        statedict['one'] = {}
+        statedict['one'] = {'write':onewrite,'dir':onelr, 'next':onenext}
         states.append(statedict)
-    print states
-    return states
+    return startstate, steps, states
+
+def runstep(strip, startstate, states, startpos):
+    for state in states:
+        if state['name'] == startstate:
+            # read strip
+            if strip[startpos] == 0:
+                instructions = state['zero']
+            else:
+                instructions = state['one']
+            #print instructions
+            # write strip
+            strip[startpos] = instructions['write']
+            # update state
+            newstate = instructions['next']
+            # move strip
+            if instructions['dir'] == 'left':
+                if startpos != 0:
+                    startpos = startpos - 1
+                else:
+                    strip.appendleft(0)
+            else:
+                startpos = startpos + 1
+                if startpos == len(strip):
+                    strip.append(0)
+
+    startstate = newstate
+    return startpos, startstate
 
 def solve1(inlist):
-    parseinput(inlist)
-    return 
+    startstate, steps, states = parseinput(inlist)
+    startpos = 0
+    strip = collections.deque()
+    strip.append(0)
+    print states
+    for i in range(steps):
+        startpos, startstate = runstep(strip, startstate, states, startpos)
+        #print startstate, startpos, strip
+
+    return strip.count(1)
 
 def solve2(inlist):
     return
 
-with open('20in.txt', 'r') as infile:
+with open('25in.txt', 'r') as infile:
     test = infile.read()
 
-test = sample1
+#test = sample1
 
 print test
 print solve1(test)
