90bfa6a895b9350519b2873f44d9b9116ad1cea8
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-01-10 17:39:19 +0000

    17 complete

--- AoC/2017/17.py
+++ AoC/2017/17.py
@@ -1,22 +1,40 @@
-sample1 ='''flqrgnkx'''
+sample1 ='''3'''
 
 
-def solve1(inlist):
-    return
+def solve1(rotations):
+    spins = 2017
+    #spins = 9
+    spinlock = [0]
+    current = 1
+    for i in range(spins):
+        current = (current + rotations) % len(spinlock)
+        #print current
+        spinlock.insert(current+1,i+1)
+        current = current + 1
+        #print spinlock
+    return spinlock[current+1]
 
-def solve2(inlist):
-    return
+def solve2(rotations):
+    spins = 50000000
 
+    #spins = 9
+    size = 1
+    current = 1
+    result = 0
+    for i in range(spins):
+        current = (current + rotations) % size
+        if current == 0:
+            #print i + 1
+            result = i + 1
+        current = current + 1
+        size = size + 1
 
-test = "flqrgnkx"
+    return result
 
-test = sample1
-
-
-with open('inputs/17in.txt', 'r') as infile:
-    tests = infile.read()
+#test = 3
+test = 329
 
 #print test
-print solve1(test)
-#print solve2(test)
+#print solve1(test)
+print solve2(test)
 
