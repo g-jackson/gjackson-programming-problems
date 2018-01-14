bfa1c4646d20e7d5aad579fbbf0522fa757a256a
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-01-14 21:04:48 +0000

    16-2 Completed

--- AoC/2017/16.py
+++ AoC/2017/16.py
@@ -1,11 +1,11 @@
 sample1 ='''s1,x3/4,pe/b'''
 
 
-def solve1(inlist):
-    order = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
+def solve1(inlist, order):
+    #order = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
     #print order
     for move in inlist:
-        print move
+        #print move
         if move[0] == 's':
             spin = len(order) - (int)(move[1:])
             order = order[spin:]+order[:spin]
@@ -23,17 +23,37 @@ def solve1(inlist):
             pos2 = order.index(val2)
             order[pos2] = val1
             order[pos1] = val2
-        print order
+        #print order
 
             
     outstring = ''
     for char in order:
         outstring = outstring + char
+    return outstring, order
+
+
+def solve2(inlist, order):
+    cycles = ['abcdefghijklmnop']
+    neworder = []
+    nocycle = True
+    i = 0
+    while nocycle:
+        i = i + 1
+        string, neworder = solve1(inlist, order)
+        order = neworder
+        print order
+        if string in cycles:
+            #print "cycle at", i
+            nocycle = False
+        else:
+            cycles.append(string)
+
+    final = cycles[1000000000 % i]
+    outstring = ''
+    for char in final:
+        outstring = outstring + char
     return outstring
 
-def solve2(inlist):
-
-    return
 
 
 
@@ -43,7 +63,8 @@ with open('inputs/16in.txt', 'r') as infile:
 #test = sample1
 test = test.split(',')
 
+order = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
 #print test
-#print solve1(test)
-print solve2(test)
+#print solve1(test, order)
+print solve2(test, order)
 
