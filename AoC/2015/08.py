26b605b5fc64392ab83cc06e2ae0eb62a59bf18d
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-01-16 12:29:24 +0000

    Fill out AoC 2015 template

---
+++ AoC/2015/08.py
@@ -0,0 +1,18 @@
+sample1=''''''
+def solve1(intext):
+
+    return 
+
+def solve2(intext):
+
+    return 
+
+
+with open('inputs/01in.txt', 'r') as infile:
+    tests = infile.read()
+
+test = sample1
+print tests
+
+print solve1(tests)
+#print solve2(tests)
