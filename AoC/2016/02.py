dd375490b01c3c63b2fb9a125c31e0ee1943e75d
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-01-18 21:26:48 +0000

    AoC 2016 2-1

--- AoC/2016/02.py
+++ AoC/2016/02.py
@@ -1,18 +1,43 @@
-sample1=''''''
-def solve1(intext):
+sample1='''ULL
+RRDDD
+LURDL
+UUUUD'''
 
-    return 
+
+def solve1(intext):
+    keypad = [[1,2,3],[4,5,6],[7,8,9]]
+    pos = [1,1]
+    passcode = ""
+    for directions in intext:      
+        for direction in directions:
+            #print direction
+            if direction == 'U':
+                if pos[0] != 0:
+                    pos[0] = pos[0] - 1
+            elif direction == 'D':
+                if pos[0] != 2:
+                    pos[0] = pos[0] + 1                
+            elif direction == 'L':
+                if pos[1] != 0:
+                    pos[1] = pos[1] - 1
+            elif direction == 'R':
+                if pos[1] != 2:
+                    pos[1] = pos[1] + 1
+            #print pos
+        passcode = passcode + (str)(keypad[pos[0]][pos[1]])
+    return passcode
 
 def solve2(intext):
 
     return 
 
 
-with open('inputs/01in.txt', 'r') as infile:
+with open('inputs/02in.txt', 'r') as infile:
     tests = infile.read()
 
-test = sample1
-print tests
+#tests = sample1
+tests = tests.split('\n')
+#print tests
 
 print solve1(tests)
 #print solve2(tests)
