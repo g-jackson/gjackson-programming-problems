5455f7c7038643ab3a9bb1b10ea241010a0e8882
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-01-22 20:07:50 +0000

    AoC 2016 18 completed

--- AoC/2016/10.py
+++ AoC/2016/10.py
@@ -1,4 +1,10 @@
-sample1=''''''
+sample1='''value 5 goes to bot 2
+bot 2 gives low to bot 1 and high to bot 0
+value 3 goes to bot 1
+bot 1 gives low to output 1 and high to bot 0
+bot 0 gives low to output 2 and high to output 0
+value 2 goes to bot 2'''
+
 def solve1(intext):
 
     return 
@@ -8,10 +14,10 @@ def solve2(intext):
     return 
 
 
-with open('inputs/01in.txt', 'r') as infile:
+with open('inputs/10in.txt', 'r') as infile:
     tests = infile.read()
 
-test = sample1
+tests = sample1
 print tests
 
 print solve1(tests)
--- AoC/2016/18.py
+++ AoC/2016/18.py
@@ -1,18 +1,102 @@
-sample1=''''''
-def solve1(intext):
+sample1='''..^^.'''
+sample2='''.^^.^.^^^^'''
 
-    return 
+def istrap(l,m,r):
+    trap = False
+    if l == "^" and m == "^" and r == ".":
+        trap = True
+    if l == "." and m == "^" and r == "^":
+        trap = True
+    if l == "^" and m == "." and r == ".":
+        trap = True
+    if l == "." and m == "." and r == "^":
+        trap = True
+    return trap
+
+def solve1(intext):
+    genrows = 40
+    rows = [list(intext)]
+    print rows
+    for row in range(genrows-1):
+        rows.append([])
+        newrow = rows[row+1]
+        oldrow = rows[row]
+        for char in range(len(rows[row])):
+            l = 0
+            m = 0
+            r = 0
+            if char == 0:
+                l = '.'
+                m = rows[row][char]
+                r = rows[row][char+1]
+            elif char == len(rows[row])-1:
+                l = rows[row][char-1]
+                m = rows[row][char]
+                r = '.'
+            else:
+                l = rows[row][char-1]
+                m = rows[row][char]
+                r = rows[row][char+1]
+            print l, m, r
+            if istrap(l,m,r):
+                print "trap"
+                newrow.append('^')
+            else:
+                print "not trap"
+                newrow.append('.')
+
+    total = 0
+    for row in rows:
+        print ''.join(row)
+        total = total + row.count(".")
+    return total
 
 def solve2(intext):
+    genrows = 400000
+    rows = [list(intext)]
+    #print rows
+    for row in range(genrows-1):
+        rows.append([])
+        newrow = rows[row+1]
+        oldrow = rows[row]
+        for char in range(len(rows[row])):
+            l = 0
+            m = 0
+            r = 0
+            if char == 0:
+                l = '.'
+                m = rows[row][char]
+                r = rows[row][char+1]
+            elif char == len(rows[row])-1:
+                l = rows[row][char-1]
+                m = rows[row][char]
+                r = '.'
+            else:
+                l = rows[row][char-1]
+                m = rows[row][char]
+                r = rows[row][char+1]
+            #print l, m, r
+            if istrap(l,m,r):
+                #print "trap"
+                newrow.append('^')
+            else:
+                #print "not trap"
+                newrow.append('.')
+        # for matchrow in range(len(rows)-1):
+        #     if newrow == rows[matchrow]:
+        #         print matchrow
+    total = 0
+    for row in rows:
+        #print ''.join(row)
+        total = total + row.count(".")
+    return total
 
-    return 
 
-
-with open('inputs/01in.txt', 'r') as infile:
+with open('inputs/18in.txt', 'r') as infile:
     tests = infile.read()
 
-test = sample1
-print tests
+#tests = sample2
+#print tests
 
-print solve1(tests)
-#print solve2(tests)
+#print solve1(tests)
+print solve2(tests)
