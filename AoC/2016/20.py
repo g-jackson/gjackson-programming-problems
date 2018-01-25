085a0d45cbdb77d46aed68d723fa08d2d8a4f59b
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-01-25 01:18:52 +0000

    AoC 2016-20-1 Completed

--- AoC/2016/20.py
+++ AoC/2016/20.py
@@ -1,7 +1,23 @@
-sample1=''''''
-def solve1(intext):
+from collections import OrderedDict
+sample1='''5-8
+0-2
+4-7'''
 
-    return 
+def solve1(intext):
+    intext = sorted(intext)
+    inlist = []
+    for line in intext:
+        line = line.split('-')
+        inlist.append(((int)(line[0]), (int)(line[1])))
+    inlist = sorted(inlist, key=lambda x: x[1])
+    lowest = 0
+    for blocked in inlist:
+        #print "lowest", lowest, "blocked range", blocked
+        if lowest >= blocked[0]:
+            if blocked[1] + 1 > lowest:              
+                lowest = blocked[1] + 1
+    return lowest
+
 
 def solve2(intext):
 
@@ -11,8 +27,9 @@ def solve2(intext):
 with open('inputs/20in.txt', 'r') as infile:
     tests = infile.read()
 
-tests = sample1
-print tests
+#tests = sample1
+#print tests
+tests = tests.split('\n')
 
 print solve1(tests)
 #print solve2(tests)
