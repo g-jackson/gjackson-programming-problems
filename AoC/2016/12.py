3a357548c70d4d821a07bb66a77df5ee75e6d6ac
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-01-28 23:57:11 +0000

    AoC2016-16-1 Started

--- AoC/2016/12.py
+++ AoC/2016/12.py
@@ -6,7 +6,7 @@ jnz a 2
 dec a'''
 
 def solve1(intext):
-    registers = {'a':0,'b':0,'c':1,'d':0}
+    registers = {'a':0,'b':0,'c':0,'d':0}
     pc = 0
     while pc < len(intext):
         line = intext[pc]
@@ -41,8 +41,39 @@ def solve1(intext):
     return registers['a']
 
 def solve2(intext):
+    registers = {'a':0,'b':0,'c':1,'d':0}
+    pc = 0
+    while pc < len(intext):
+        line = intext[pc]
+        line = line.split()
+        #print registers
+        #print line
+        instruction = line[0]
+        if instruction == "inc":
+            registers[line[1]] = registers[line[1]] + 1
+        if instruction == "dec":
+            registers[line[1]] = registers[line[1]] - 1
+        if instruction == "cpy":
+            if line[1].islower():
+                registers[line[2]] = registers[line[1]]
+            else:
+                registers[line[2]] = (int)(line[1])
+        if instruction == "jnz":
+            if line[1].islower():
+                if registers[line[1]] != 0:
+                    pc = pc + (int)(line[2])
+                else:
+                    pc = pc + 1  
+            else:
+                if (int)(line[1]) != 0:
+                    pc = pc + (int)(line[2])
+                else:
+                    pc = pc + 1  
+        else:
+            pc = pc + 1    
 
-    return 
+    print registers
+    return registers['a']
 
 
 with open('inputs/12in.txt', 'r') as infile:
--- AoC/2016/16.py
+++ AoC/2016/16.py
@@ -1,6 +1,22 @@
-sample1=''''''
-def solve1(intext):
+sample1='''111100001010'''
 
+def generatecurve(initial, endlength):
+    outstring = initial
+    while len(outstring) < endlength:
+        forward = (str)(outstring)
+        forward = forward[2:]
+        backward = initial[:1:-1]
+        backward = backward.replace("0", "2")
+        backward = backward.replace("1", "0")
+        backward = backward.replace("2", "1")
+        outstring = forward + "0" + backward
+    print outstring
+
+def solve1(intext):
+    length = 15#272
+    initial = bin(int(intext, 2))
+    #print initial
+    generatecurve(initial, length)
     return 
 
 def solve2(intext):
@@ -12,7 +28,6 @@ with open('inputs/16in.txt', 'r') as infile:
     tests = infile.read()
 
 tests = sample1
-print tests
 
 print solve1(tests)
 #print solve2(tests)
