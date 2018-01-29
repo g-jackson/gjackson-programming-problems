23ad913ac62360a43ebb174fdf0ed3ad6a399ed1
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-01-29 00:36:21 +0000

    AoC2016-16 Completed

--- AoC/2016/16.py
+++ AoC/2016/16.py
@@ -1,23 +1,37 @@
-sample1='''111100001010'''
+sample1='''10000'''
 
-def generatecurve(initial, endlength):
+def gencurve(initial, endlength):
     outstring = initial
-    while len(outstring) < endlength:
-        forward = (str)(outstring)
-        forward = forward[2:]
-        backward = initial[:1:-1]
+    while len(outstring) <= endlength:
+        forward = outstring
+        backward = forward[::-1]
         backward = backward.replace("0", "2")
         backward = backward.replace("1", "0")
         backward = backward.replace("2", "1")
         outstring = forward + "0" + backward
-    print outstring
+        #print "fw", forward, "bw", backward, "total", outstring, "len", len(outstring)
+    return outstring[:endlength]
+
+def genchecksum(inputstring):
+    checksum = inputstring
+    while len(checksum) % 2 == 0:
+        newchecksum = ""
+        for i in range(len(checksum)/2):
+            if checksum[i*2] == checksum[(i*2)+1]:
+                newchecksum += "1"
+            else:
+                newchecksum += "0"
+        checksum = newchecksum
+        #print len(newchecksum), newchecksum
+    return checksum
 
 def solve1(intext):
-    length = 15#272
-    initial = bin(int(intext, 2))
+    length = 35651584
     #print initial
-    generatecurve(initial, length)
-    return 
+    generated = gencurve(intext, length)
+    #print "gen", generated
+    checksum = genchecksum(generated)
+    return checksum
 
 def solve2(intext):
 
@@ -27,7 +41,7 @@ def solve2(intext):
 with open('inputs/16in.txt', 'r') as infile:
     tests = infile.read()
 
-tests = sample1
+#tests = sample1
 
 print solve1(tests)
 #print solve2(tests)
