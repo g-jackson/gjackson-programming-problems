a608c2d7738fea7410c98322187a1d87b60c1c95
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-02-02 23:36:51 +0000

    AoC2016-14-1 Completed

--- AoC/2016/14.py
+++ AoC/2016/14.py
@@ -1,7 +1,33 @@
-sample1=''''''
-def solve1(intext):
+import hashlib
+import re
+sample1='''abc'''
 
-    return 
+def solve1(intext):
+    keys = []
+    counter = 0
+    result = 0
+    while len(keys) < 64:
+        hashing = intext + (str)(counter)
+        hashed = hashlib.md5(hashing).hexdigest()
+        recheck = re.compile('(.)\\1{2}').search(hashed)
+        if recheck:
+            #print hashing
+            valid = False
+            findchar = recheck.group(0)[0]
+            for i in range(1000):
+
+                subhashing = intext + (str)(counter+i+1)
+                subhashed = hashlib.md5(subhashing).hexdigest()
+                subrecheck = re.compile(r'%s{5}'%findchar).search(subhashed)
+                if subrecheck and not valid:
+                    print len(keys), findchar, hashing, hashed, subhashing, subhashed
+                    result = counter
+                    valid = True
+            if valid:
+                keys.append(hashed)
+
+        counter = counter + 1
+    return result
 
 def solve2(intext):
 
@@ -11,7 +37,7 @@ def solve2(intext):
 with open('inputs/14in.txt', 'r') as infile:
     tests = infile.read()
 
-tests = sample1
+#tests = sample1
 print tests
 
 print solve1(tests)
