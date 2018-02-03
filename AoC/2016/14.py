badc9a9df7770e3434cef6107a2491cff77dc677
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-02-03 00:01:11 +0000

    AoC2016-14-2 Completed

--- AoC/2016/14.py
+++ AoC/2016/14.py
@@ -15,7 +15,6 @@ def solve1(intext):
             valid = False
             findchar = recheck.group(0)[0]
             for i in range(1000):
-
                 subhashing = intext + (str)(counter+i+1)
                 subhashed = hashlib.md5(subhashing).hexdigest()
                 subrecheck = re.compile(r'%s{5}'%findchar).search(subhashed)
@@ -29,10 +28,43 @@ def solve1(intext):
         counter = counter + 1
     return result
 
+def hash(memory, value):
+    hashed = ''
+    if value not in memory:
+        hashed = hashlib.md5(value).hexdigest()
+        for i in range(2016):
+            hashed = hashlib.md5(hashed).hexdigest()
+        memory[value] = hashed
+        return hashed
+    else:
+        return memory[value]
+
 def solve2(intext):
+    memory = {}
+    keys = []
+    counter = 0
+    result = 0
+    while len(keys) < 64:
+        hashing = intext + (str)(counter)
+        hashed = hash(memory,hashing)
+        recheck = re.compile('(.)\\1{2}').search(hashed)
+        if recheck:
+            #print hashing
+            valid = False
+            findchar = recheck.group(0)[0]
+            for i in range(1000):
+                subhashing = intext + (str)(counter+i+1)
+                subhashed = hash(memory,subhashing)
+                subrecheck = re.compile(r'%s{5}'%findchar).search(subhashed)
+                if subrecheck and not valid:
+                    print len(keys), findchar, hashing, hashed, subhashing, subhashed
+                    result = counter
+                    valid = True
+            if valid:
+                keys.append(hashed)
 
-    return 
-
+        counter = counter + 1
+    return result
 
 with open('inputs/14in.txt', 'r') as infile:
     tests = infile.read()
@@ -40,5 +72,5 @@ with open('inputs/14in.txt', 'r') as infile:
 #tests = sample1
 print tests
 
-print solve1(tests)
-#print solve2(tests)
+#print solve1(tests)
+print solve2(tests)
