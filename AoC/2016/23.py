dee44f6451f3b985f9243b7c1d22e200bede2acf
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-02-06 18:56:03 +0000

    AoC2016-23-2 Completed

--- AoC/2016/23.py
+++ AoC/2016/23.py
@@ -1,3 +1,4 @@
+import math
 sample1='''cpy 2 a
 tgl a
 tgl a
@@ -8,6 +9,65 @@ dec a'''
 
 def solve1(intext):
     registers = {'a':7,'b':0,'c':0,'d':0}
+    pc = 0
+    while pc < len(intext):
+        line = intext[pc]
+        line = line.split()
+        print registers
+        print line
+        instruction = line[0]
+        if instruction == "inc":
+            registers[line[1]] = registers[line[1]] + 1
+        
+        if instruction == "dec":
+            registers[line[1]] = registers[line[1]] - 1
+        
+        if instruction == "cpy":
+            if line[1].islower():
+                registers[line[2]] = registers[line[1]]
+            else:
+                registers[line[2]] = (int)(line[1])
+        
+        if instruction == "tgl":
+            ininstruction = ""
+            if pc + registers[line[1]] >= 0 and pc + registers[line[1]] < len(intext):
+                ininstruction = intext[pc + registers[line[1]]]
+                outinstruction = ""
+                if "inc" in ininstruction:
+                    outinstruction = "dec " + "".join(ininstruction.split()[1:])
+                if "dec" in ininstruction:
+                    outinstruction = "inc " + "".join(ininstruction.split()[1:])
+                if "tgl" in ininstruction:
+                    outinstruction = "inc " + "".join(ininstruction.split()[1:])
+                if "jnz" in ininstruction:
+                    outinstruction = "cpy " + " ".join(ininstruction.split()[1:])
+                if "cpy" in ininstruction:
+                    outinstruction = "jnz " + " ".join(ininstruction.split()[1:])
+                #print "toggling", intext[pc + registers[line[1]]], "to", outinstruction
+                intext[pc + registers[line[1]]] = outinstruction
+        
+        if instruction == "jnz":
+            if line[1].islower():
+                if registers[line[1]] != 0:
+                    pc = pc + (int)(line[2])
+                else:
+                    pc = pc + 1  
+            else:
+                if (int)(line[1]) != 0:
+                    if line[2].islower():
+                        pc = pc + registers[line[2]]
+                    else:
+                        pc = pc + (int)(line[2])
+                else:
+                    pc = pc + 1
+        else:
+            pc = pc + 1    
+
+    print registers
+    return registers['a']
+
+def solve2a(intext):
+    registers = {'a':12,'b':0,'c':0,'d':0}
     pc = 0
     while pc < len(intext):
         line = intext[pc]
@@ -65,17 +125,16 @@ def solve1(intext):
     print registers
     return registers['a']
 
-def solve2(intext):
-
-    return 
-
+def solve2b():
+    return math.factorial(12) + (81*92)
 
 with open('inputs/23in.txt', 'r') as infile:
     tests = infile.read()
 
 #tests = sample1
-print tests
+#print tests
 tests = tests.split('\n')
 
-print solve1(tests)
-#print solve2(tests)
+#print solve1(tests)
+#print solve2a(tests)
+print solve2b()
