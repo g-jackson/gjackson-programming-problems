fcc24d8c1fecf105cf8575da7de5744bec603bd0
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-02-10 20:54:01 +0000

    AoC2015-8 Completed

--- AoC/2015/08.py
+++ AoC/2015/08.py
@@ -1,18 +1,30 @@
-sample1=''''''
-def solve1(intext):
+sample1='''""
+"abc"
+"aaa\"aaa"
+"\x27"'''
 
-    return 
+def solve1(intext):
+    acutalchars = len(intext)
+    evalchars = 0
+    split = intext.split()
+    for line in split:
+        evalchars += len(eval(line))
+    evalchars += len(split) - 1 # Add in newline chars removed by split
+    return acutalchars - evalchars
 
 def solve2(intext):
-
-    return 
+    addedchars = 0
+    split = intext.split()
+    for line in split:
+        addedchars += 2 + line.count('\\') + line.count('"')
+    return addedchars
 
 
 with open('inputs/08in.txt', 'r') as infile:
     tests = infile.read()
 
-tests = sample1
-print tests
+#tests = sample1
+#print tests
 
 print solve1(tests)
-#print solve2(tests)
+print solve2(tests)
