376859adebd06bed84c7f50fc97878d43087b8f6
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-02-11 11:45:58 +0000

    AoC2015-11 Completed

--- AoC/2015/11.py
+++ AoC/2015/11.py
@@ -1,18 +1,54 @@
-sample1=''''''
+sample1='''hijklmmn'''
+
+def increment(password):
+    incremented = list(password[::-1])
+    done = False
+    for i in range(len(incremented)):
+        if not done:
+            if incremented[i] != 'z':
+                done = True
+                incremented[i] = chr(ord(incremented[i])+1)
+            else:
+                incremented[i] = 'a'
+    return incremented[::-1]
+
+def increasing(password):
+    for i in range(len(password)-2):
+        if ord(password[i]) == ord(password[i+1])-1 and ord(password[i]) == ord(password[i+2])-2:
+            return True
+    return False
+
+def banned(password):
+    if 'i' in password or 'o' in password or 'l' in password:
+        return False
+    return True
+
+def pairs(password):
+    first = -1
+    for i in range(len(password)-1):
+        if password[i] == password[i+1] and i != first:
+            if first == -1:
+                first = i + 1
+            else:
+                return True
+    return False
+
 def solve1(intext):
-
-    return 
-
-def solve2(intext):
-
-    return 
+    password = list(intext)
+    done = increasing(password) and banned(password) and pairs(password)
+    while not done:
+        password = increment(password)
+        done = increasing(password) and banned(password) and pairs(password)
+        #print ''.join(password)
+    return ''.join(password) 
 
 
 with open('inputs/11in.txt', 'r') as infile:
     tests = infile.read()
 
-tests = sample1
+#tests = sample1
 print tests
 
-print solve1(tests)
-#print solve2(tests)
+a = solve1(tests)
+print a
+print solve1(increment(a))
