cde59b5f39813ed6999812ce2e6b4aaf67ff34e2
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-02-11 12:28:35 +0000

    AoC2015-12-1 Completed

--- AoC/2015/12.py
+++ AoC/2015/12.py
@@ -1,7 +1,12 @@
-sample1=''''''
-def solve1(intext):
+import re
 
-    return 
+sample1=''''''
+
+def solve1(intext):
+    removed = re.findall('-?[0-9]\d*',intext)
+    removed = map(int, removed)
+    #print removed
+    return sum(removed)
 
 def solve2(intext):
 
@@ -11,8 +16,9 @@ def solve2(intext):
 with open('inputs/12in.txt', 'r') as infile:
     tests = infile.read()
 
-tests = sample1
-print tests
+
+#tests = sample1
+#print tests
 
 print solve1(tests)
 #print solve2(tests)
