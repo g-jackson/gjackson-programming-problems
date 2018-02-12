ac314bf66b5675af6da6da44b7d48a354b2fbd42
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-02-12 22:59:59 +0000

    AoC2015-24-1 Completed

--- AoC/2015/24.py
+++ AoC/2015/24.py
@@ -1,7 +1,57 @@
-sample1=''''''
-def solve1(intext):
+from itertools import combinations
+from operator import mul
 
-    return 
+sample1='''1
+2
+3
+4
+5
+7
+8
+9
+10
+11'''
+
+def splitter(weights, split, maxsize=0, minsize=1):
+    if maxsize == 0:
+        maxsize = len(weights)
+
+    combos = []
+    for i in range(minsize,maxsize+1):
+        for j in combinations(weights,i):
+            if sum(j) == split:    
+                combos.append(list(j))
+    return combos
+
+def solve1(intext):
+    weights =  map(int, intext)[::-1]
+    maxmiddle = len(weights)/3
+    split = sum(weights)/3
+    print weights, split, maxmiddle
+    middlecombos = splitter(weights, split, maxsize=maxmiddle)
+    minpackages = len(weights)
+    minqe = reduce(mul, weights, 1)
+    smallest = []
+    counter = 0
+    print len(middlecombos)
+    for mcombo in middlecombos:
+        print counter
+        counter += 1
+        lremaining = [i for i in weights if i not in mcombo]
+        leftcombos = splitter(lremaining,split, maxsize=(len(weights)-(len(mcombo)))/2,minsize=len(mcombo))
+        for lcombo in leftcombos:
+            rcombo = [i for i in weights if i not in mcombo and i not in lcombo]
+            #print mcombo, lcombo, rcombo
+            combo = (mcombo, lcombo, rcombo)
+            if len(combo[0]) <= minpackages:
+                minpackages = len(combo[0])
+                if reduce(mul, combo[0], 1) < minqe:
+                    minqe = reduce(mul, combo[0], 1)
+                    smallest = combo
+                    print "minqe:", minqe
+
+    print smallest, minqe, minpackages
+    return minqe
 
 def solve2(intext):
 
@@ -11,8 +61,8 @@ def solve2(intext):
 with open('inputs/24in.txt', 'r') as infile:
     tests = infile.read()
 
-tests = sample1
-print tests
-
+#tests = sample1
+#print tests
+tests = tests.split('\n')
 print solve1(tests)
 #print solve2(tests)
