f15b2a26b65c29d6035ef8b88d0eede63c6c4012
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-02-12 14:06:25 +0000

    AoC2015-16 Completed

--- AoC/2015/16.py
+++ AoC/2015/16.py
@@ -1,18 +1,65 @@
 sample1=''''''
-def solve1(intext):
 
-    return 
+def readinput():
+    facts = {"children": 3, "cats": 7, "samoyeds": 2, "pomeranians": 3, "akitas": 0, "vizslas": 0, "goldfish": 5, "trees": 3, "cars": 2, "perfumes": 1}
+    return facts
+
+def solve1(intext):
+    #print intext
+    facts = readinput()
+    #print facts
+    result = 0
+    for line in intext:
+        split = line.split()
+        sue = split[1][:-1]
+        elements = (len(split)/2)-1
+        #print sue, len(split), elements, split
+        valid = True
+        for i in range(elements):
+            key = split[2+(2*i)][:-1]
+            value = (int)(split[3+(2*i)].replace(',', ''))
+            if facts[key] != value:
+                #print key, value, facts
+                valid = False
+        if valid:
+            result = sue 
+    return result
 
 def solve2(intext):
+    #print intext
+    facts = readinput()
+    #print facts
+    result = 0
+    for line in intext:
+        split = line.split()
+        sue = split[1][:-1]
+        elements = (len(split)/2)-1
+        #print sue, len(split), elements, split
+        valid = True
+        for i in range(elements):
+            key = split[2+(2*i)][:-1]
+            value = (int)(split[3+(2*i)].replace(',', ''))
+            if key == "cats" or key == "trees":
+                if facts[key] >= value:
+                    valid = False
+            elif key == "pomeranians" or key == "goldfish":
+                if facts[key] <= value:
+                    valid = False
+            else:
+                if facts[key] != value:   
+                    valid = False
+        if valid:
+            #print split, facts
+            result = sue 
+    return result
 
-    return 
 
-
-with open('inputs/16in.txt', 'r') as infile:
+with open('inputs/16in.txt','r') as infile:
     tests = infile.read()
 
-tests = sample1
-print tests
+#tests = sample1
+#print tests
+tests = tests.split('\n')
 
 print solve1(tests)
-#print solve2(tests)
+print solve2(tests)
--- AoC/2015/18.py
+++ AoC/2015/18.py
@@ -1,6 +1,34 @@
 sample1=''''''
-def solve1(intext):
 
+def printlights(board):
+    maxx = 0
+    maxy = 0
+    for i in board:
+        if maxx < i[0]:
+            maxx = i[0]
+        if maxy < i[1]:
+            maxy = i[1]        
+
+    for y in range(maxy+1):
+        for x in range(maxx+1):
+            if (x,y) in board:
+                print '#',
+            else:
+                print '.',
+        print ""
+
+def readinput(intext):
+    lights = []
+    for y in range(len(intext)):
+        for x in range(len(intext[y])):
+            if intext[y][x] == '#':
+                lights.append((x,y))
+    return lights
+
+def solve1(intext):
+    lights = readinput(intext)
+    #print lights
+    printlights(lights)
     return 
 
 def solve2(intext):
@@ -11,8 +39,9 @@ def solve2(intext):
 with open('inputs/18in.txt', 'r') as infile:
     tests = infile.read()
 
-tests = sample1
-print tests
+#tests = sample1
+#print tests
+tests = tests.split('\n')
 
 print solve1(tests)
 #print solve2(tests)
