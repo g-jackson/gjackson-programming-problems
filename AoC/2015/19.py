925279e3d3d71d1cf743d1504639a5402ec41932
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-02-18 21:12:05 +0000

    AoC2015-19-1 Completed

--- AoC/2015/19.py
+++ AoC/2015/19.py
@@ -1,7 +1,35 @@
-sample1=''''''
-def solve1(intext):
+import re
+sample1='''H => HO
+H => OH
+O => HH
 
-    return 
+HOHOHO'''
+def solve1(intext):
+    startstring = intext[-1]
+    replacements = intext[0:-2]
+    replace = {}
+    for replacement in replacements:
+        fromstring = replacement.split(' => ')[0]
+        tostring = replacement.split(' => ')[1]
+        if fromstring not in replace:
+            replace[fromstring] = [tostring]
+        else:
+            replace[fromstring].append(tostring)
+    print replace
+    newmolecules = []
+    for r in replace:
+        starts = [match.start() for match in re.finditer(re.escape(r), startstring)]
+        matches = replace[r]
+        for match in matches:
+            for start in starts:
+                newmolecule = "".join([startstring[0:start] , match , startstring[start+len(r):]])
+                #print "found", r, "=>", match, "start:", start, "newmolecule:", newmolecule
+                #print startstring[0:start] , match , startstring[start+1:]
+                if newmolecule not in newmolecules:
+                    newmolecules.append(newmolecule)
+    return len(newmolecules)
+
+#615
 
 def solve2(intext):
 
@@ -11,8 +39,8 @@ def solve2(intext):
 with open('inputs/19in.txt', 'r') as infile:
     tests = infile.read()
 
-tests = sample1
-print tests
-
+#tests = sample1
+#print tests
+tests = tests.split('\n')
 print solve1(tests)
 #print solve2(tests)
