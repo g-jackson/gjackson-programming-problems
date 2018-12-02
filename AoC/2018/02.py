e71bed522265ac868bd9ae5ac6fa1ad293602d36
g-jackson <1174498+g-jackson@users.noreply.github.com>
2018-12-02 14:39:34 +0000

    Day2 P1 Completed

--- AoC/2018/02.py
+++ AoC/2018/02.py
@@ -1,16 +1,38 @@
-testinputs = [-6, +3, +8, +5, -6]
+testinputs = ["abcdef","bababc","abbcde","abcccd","aabcdd","abcdee","ababab"]
 
 def a(inputs):
-    return
+    sumtwo = 0
+    sumthree = 0
+    for input in inputs:
+        addtwo = False
+        addthree = False
+        for char in range(0,26):
+            if count_letters(input, char) == 2:
+                addtwo = True
+            if count_letters(input, char) == 3:
+                addthree = True
+        if addtwo:
+            sumtwo += 1
+        if addthree:
+            sumthree += 1
+        print input, addtwo, addthree, sumtwo, sumthree
+    return sumtwo * sumthree
+
+
+def count_letters(word, char):
+    char = chr(97+char)
+    return sum(char == c for c in word)
+
 
 def b(inputs):
     return
 
 
+
 with open('inputs/02in.txt', 'r') as infile:
     inputs = infile.read()
 inputs = inputs.split()
+print inputs
 
-
-print a(inputs)
+#print a(inputs)
 print b(inputs)
