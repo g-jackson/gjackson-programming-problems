6f9419d04fccc5dce587fd006fd1bf06d62087d8
g-jackson <1174498+g-jackson@users.noreply.github.com>
2019-10-28 16:16:39 +0000

    Day 7 Complete

--- AoC/2018/06.py
+++ AoC/2018/06.py
@@ -1,16 +1,42 @@
-ddtestinputs = [-6, +3, +8, +5, -6]
+testinputs = """1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9"""
 
 def a(inputs):
-    return
+    points = []
+    for i in inputs:
+        points.append((int(i.split(", ")[0]), int(i.split(", ")[1])))
+    print(points)
+    print(gen_perimeter(points))
+    return inputs
 
 def b(inputs):
     return
 
 
+def gen_perimeter(points):
+    x_max = points[0]
+    x_min = points[0]
+    y_max = points[0]
+    y_min = points[0]
+    for point in points:
+        if point[0] > x_min[0]:
+            x_min = point
+        if point[0] < x_max[0]:
+            x_max = point
+        if point[1] > y_min[1]:
+            y_min = point
+        if point[1] < y_max[1]:
+            y_max = point
+    return x_max, x_min, y_max, y_min
+
+
 with open('inputs/06in.txt', 'r') as infile:
     inputs = infile.read()
-inputs = inputs.split()
+inputs = inputs.split("\n")
 
-
-print a(inputs)
-print b(inputs)
+print(a(testinputs.split("\n")))
+print(b(inputs))
--- AoC/2018/07.py
+++ AoC/2018/07.py
@@ -1,16 +1,95 @@
-testinputs = [-6, +3, +8, +5, -6]
+testinputs = """Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin."""
 
 def a(inputs):
-    return
+    dependancies = {}
+    for input in inputs:
+        before = input.split()[1]
+        after = input.split()[7]
+        if before not in dependancies:
+            dependancies[before] = []
+        if after not in dependancies:
+            dependancies[after] = [before]
+        else:
+            dependancies[after].append(before)
+
+    result = ""
+    # Remove nodes until none remain
+    for _ in range(len(dependancies)):
+        remove_letter = "Z"
+        # Find next node to remove
+        for node in dependancies:
+            if dependancies[node] == [] and node < remove_letter:
+                remove_letter = node
+
+        result += remove_letter
+
+        # Remove node and remove from all dependancies
+        del dependancies[remove_letter]
+        for node in dependancies:
+            if remove_letter in dependancies[node]:
+                dependancies[node].remove(remove_letter)
+
+    return result
 
 def b(inputs):
-    return
+    dependancies = {}
+    for input in inputs:
+        before = input.split()[1]
+        after = input.split()[7]
+        if before not in dependancies:
+            dependancies[before] = []
+        if after not in dependancies:
+            dependancies[after] = [before]
+        else:
+            dependancies[after].append(before)
+
+    second = 0
+    workers = 5
+    time_remaining = {}
+    while (dependancies != {}) or (time_remaining != {}):
+        # Start tasks
+        task_ready = True
+        while workers != 0 and task_ready:
+            remove_letter = chr(91)
+            for node in dependancies:
+                if dependancies[node] == [] and node < remove_letter:
+                    remove_letter = node
+            if remove_letter == chr(91):
+                task_ready = False
+            else:
+                workers -= 1
+                del dependancies[remove_letter]
+                time_remaining[remove_letter] = ord(remove_letter) - 64 + 60
+        
+        # print(second, time_remaining, dependancies)
+        # Count down any tasks in progress
+        del_nodes = []
+        for node in time_remaining:
+            time_remaining[node] -= 1
+            # Remove from all dependancies if task done
+            if time_remaining[node] == 0:
+                for dependancy in dependancies:
+                    if node in dependancies[dependancy]:
+                        dependancies[dependancy].remove(node)
+                del_nodes = node
+                workers += 1
+        for node in del_nodes:
+            del time_remaining[node]
+        second += 1
+    return second
 
 
 with open('inputs/07in.txt', 'r') as infile:
     inputs = infile.read()
-inputs = inputs.split()
+inputs = inputs.split("\n")
 
 
-print a(inputs)
-print b(inputs)
+#print(a(inputs))
+#print(b(testinputs.split("\n")))
+print(b(inputs))
