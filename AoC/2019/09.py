bf2abfa5b1e56294ba0cfa0723ea058344414a79
g-jackson <1174498+g-jackson@users.noreply.github.com>
2020-01-02 19:01:37 +0000

    10 part 1 Complete

---
+++ AoC/2019/09.py
@@ -0,0 +1,22 @@
+testinputs = []
+
+
+def a(inputs):
+    return
+
+
+def b(inputs):
+    return
+
+
+with open('inputs/09in.txt', 'r') as infile:
+    inputs = infile.read()
+inputs = inputs.split()
+
+for i in range(len(inputs)):
+    inputs[i] = int(inputs[i])
+
+
+print(a(testinputs))
+#print(b(inputs))
+#print tests
